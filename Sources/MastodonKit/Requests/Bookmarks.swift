53d4466e67d28b99115d12443bfbf67a6297a110
Sören Kuklau <kuklau@trenz.de>
2022-12-03 23:10:27 +0100

    list support for bookmarks

---
+++ Sources/MastodonKit/Requests/Bookmarks.swift
@@ -0,0 +1,22 @@
+//
+//  Bookmarks.swift
+//  MastodonKit
+//
+//  Created by Sören Kuklau on 03.12.22.
+//
+
+import Foundation
+
+/// `Bookmarks` requests.
+public enum Bookmarks {
+    /// Fetches a user's bookmarks.
+    ///
+    /// - Parameter range: The bounds used when requesting data from Mastodon.
+    /// - Returns: Request for `[Status]`.
+    public static func all(range: RequestRange = .default) -> Request<[Status]> {
+        let parameters = range.parameters(limit: between(1, and: 40, default: 20))
+        let method = HTTPMethod.get(.parameters(parameters))
+
+        return Request<[Status]>(path: "/api/v1/bookmarks", method: method)
+    }
+}
