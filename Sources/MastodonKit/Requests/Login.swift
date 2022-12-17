chucker/MastodonKit

commit 4270aa7c87c1967fa82365d6197d6493cc77d301

Use JSON payload for most requests, update tests

Sources/MastodonKit/Requests/Login.swift
@@ -24,16 +24,16 @@ public enum Login {
                               scopes: [AccessScope],
                               username: String,
                               password: String) -> Request<LoginSettings> {
-        let parameters = [
-            Parameter(name: "client_id", value: clientID),
-            Parameter(name: "client_secret", value: clientSecret),
-            Parameter(name: "scope", value: scopes.map(toString).joined(separator: " ")),
-            Parameter(name: "grant_type", value: "password"),
-            Parameter(name: "username", value: username),
-            Parameter(name: "password", value: password)
+        let parameters: [String: AnyEncodable] = [
+            "client_id": AnyEncodable(clientID),
+            "client_secret": AnyEncodable(clientSecret),
+            "scope": AnyEncodable(scopes.map(toString)),
+            "grant_type": AnyEncodable("password"),
+            "username": AnyEncodable(username),
+            "password": AnyEncodable(password)
         ]
 
-        let method = HTTPMethod.post(.parameters(parameters))
+        let method = HTTPMethod.post(.json(encoding: parameters))
         return Request<LoginSettings>(path: "/oauth/token", method: method)
     }
 
@@ -51,16 +51,16 @@ public enum Login {
                              scopes: [AccessScope],
                              redirectURI: String,
                              code: String) -> Request<LoginSettings> {
-        let parameters = [
-            Parameter(name: "client_id", value: clientID),
-            Parameter(name: "client_secret", value: clientSecret),
-            Parameter(name: "scope", value: scopes.map(toString).joined(separator: " ")),
-            Parameter(name: "grant_type", value: "authorization_code"),
-            Parameter(name: "redirect_uri", value: redirectURI),
-            Parameter(name: "code", value: code)
+        let parameters: [String: AnyEncodable] = [
+            "client_id": AnyEncodable(clientID),
+            "client_secret": AnyEncodable(clientSecret),
+            "scope": AnyEncodable(scopes.map(toString)),
+            "grant_type": AnyEncodable("authorization_code"),
+            "redirect_uri": AnyEncodable(redirectURI),
+            "code": AnyEncodable(code)
         ]
 
-        let method = HTTPMethod.post(.parameters(parameters))
+        let method = HTTPMethod.post(.json(encoding: parameters))
         return Request<LoginSettings>(path: "/oauth/token", method: method)
     }
 }

Tests/MastodonKitTests/Requests/LoginTests.swift
@@ -20,14 +20,13 @@ class LoginTests: XCTestCase {
         XCTAssertEqual(request.method.name, "POST")
         XCTAssertNil(request.method.queryItems)
 
-        let payload = String(data: request.method.httpBody!, encoding: .utf8)!
-        XCTAssertEqual(payload.components(separatedBy: "&").count, 6)
-        XCTAssertTrue(payload.contains("client_id=client%20id"))
-        XCTAssertTrue(payload.contains("client_secret=client%20secret"))
-        XCTAssertTrue(payload.contains("scope=read%20write"))
-        XCTAssertTrue(payload.contains("grant_type=password"))
-        XCTAssertTrue(payload.contains("username=foo"))
-        XCTAssertTrue(payload.contains("password=123"))
+        let payload = try! JSONSerialization.jsonObject(with: request.method.httpBody!, options: []) as! NSDictionary
+        XCTAssertEqual(payload["username"] as? String, "foo")
+        XCTAssertEqual(payload["scope"] as? [String], ["read", "write"])
+        XCTAssertEqual(payload["password"] as? String, "123")
+        XCTAssertEqual(payload["client_id"] as? String, "client id")
+        XCTAssertEqual(payload["client_secret"] as? String, "client secret")
+        XCTAssertEqual(payload["grant_type"] as? String, "password")
     }
 
     func testOAuthLogin() {
@@ -40,13 +39,12 @@ class LoginTests: XCTestCase {
         XCTAssertEqual(request.method.name, "POST")
         XCTAssertNil(request.method.queryItems)
 
-        let payload = String(data: request.method.httpBody!, encoding: .utf8)!
-        XCTAssertEqual(payload.components(separatedBy: "&").count, 6)
-        XCTAssertTrue(payload.contains("client_id=client%20id"))
-        XCTAssertTrue(payload.contains("client_secret=client%20secret"))
-        XCTAssertTrue(payload.contains("scope=read%20write"))
-        XCTAssertTrue(payload.contains("grant_type=authorization_code"))
-        XCTAssertTrue(payload.contains("redirect_uri=foo%3A//oauth"))
-        XCTAssertTrue(payload.contains("code=123"))
+        let payload = try! JSONSerialization.jsonObject(with: request.method.httpBody!, options: []) as! NSDictionary
+        XCTAssertEqual(payload["client_id"] as? String, "client id")
+        XCTAssertEqual(payload["client_secret"] as? String, "client secret")
+        XCTAssertEqual(payload["scope"] as? [String], ["read", "write"])
+        XCTAssertEqual(payload["grant_type"] as? String, "authorization_code")
+        XCTAssertEqual(payload["redirect_uri"] as? String, "foo://oauth")
+        XCTAssertEqual(payload["code"] as? String, "123")
     }
 }

Sources/MastodonKit/Requests/Accounts.swift
@@ -131,9 +131,7 @@ public enum Accounts {
     /// - Parameter uri: The 'username@domain' of the remote user to follow.
     /// - Returns: Request for `Account`.
     public static func remoteFollow(uri: String) -> Request<Account> {
-        let parameter = [Parameter(name: "uri", value: uri)]
-        let method = HTTPMethod.post(.parameters(parameter))
-
+        let method = HTTPMethod.post(.json(encoding: ["uri": uri]))
         return Request<Account>(path: "/api/v1/follows", method: method)
     }
 

Sources/MastodonKit/Requests/Clients.swift
@@ -22,14 +22,14 @@ public enum Clients {
                                 redirectURI: String = "urn:ietf:wg:oauth:2.0:oob",
                                 scopes: [AccessScope],
                                 website: String? = nil) -> Request<ClientApplication> {
-        let parameters = [
-            Parameter(name: "client_name", value: clientName),
-            Parameter(name: "redirect_uris", value: redirectURI),
-            Parameter(name: "website", value: website),
-            Parameter(name: "scopes", value: scopes.map(toString).joined(separator: " "))
+        let parameters: [String: AnyEncodable?] = [
+            "client_name": AnyEncodable(clientName),
+            "redirect_uris": AnyEncodable(redirectURI),
+            "website": website.map { AnyEncodable($0) },
+            "scopes": scopes.isEmpty ? nil : AnyEncodable(scopes)
         ]
 
-        let method = HTTPMethod.post(.parameters(parameters))
+        let method = HTTPMethod.post(.json(encoding: parameters.compactMapValues { $0 }))
         return Request<ClientApplication>(path: "/api/v1/apps", method: method)
     }
 }

Sources/MastodonKit/Requests/DomainBlocks.swift
@@ -26,8 +26,7 @@ public enum DomainBlocks {
     /// - Parameter domain: The domain to block.
     /// - Returns: Request for `Empty`.
     public static func block(domain: String) -> Request<Empty> {
-        let parameter = [Parameter(name: "domain", value: domain)]
-        let method = HTTPMethod.post(.parameters(parameter))
+        let method = HTTPMethod.post(.json(encoding: ["domain": domain]))
 
         return Request<Empty>(path: "/api/v1/domain_blocks", method: method)
     }
@@ -37,8 +36,7 @@ public enum DomainBlocks {
     /// - Parameter domain: The domain to unblock.
     /// - Returns: Request for `Empty`.
     public static func unblock(domain: String) -> Request<Empty> {
-        let parameter = [Parameter(name: "domain", value: domain)]
-        let method = HTTPMethod.delete(.parameters(parameter))
+        let method = HTTPMethod.delete(.json(encoding: ["domain": domain]))
 
         return Request<Empty>(path: "/api/v1/domain_blocks", method: method)
     }

Sources/MastodonKit/Requests/Lists.swift
@@ -38,8 +38,7 @@ public enum Lists {
     /// - Parameter title: The title of the list.
     /// - Returns: Request for `List`.
     public static func create(title: String) -> Request<List> {
-        let parameter = [Parameter(name: "title", value: title)]
-        let method = HTTPMethod.post(.parameters(parameter))
+        let method = HTTPMethod.post(.json(encoding: ["title": title]))
 
         return Request<List>(path: "/api/v1/lists", method: method)
     }
@@ -51,8 +50,7 @@ public enum Lists {
     ///   - title: The title of the list.
     /// - Returns: Request for `List`.
     public static func update(id: String, title: String) -> Request<List> {
-        let parameter = [Parameter(name: "title", value: title)]
-        let method = HTTPMethod.put(.parameters(parameter))
+        let method = HTTPMethod.put(.json(encoding: ["title": title]))
 
         return Request<List>(path: "/api/v1/lists/\(id)", method: method)
     }
@@ -72,8 +70,7 @@ public enum Lists {
     ///   - id: The list ID>
     /// - Returns: Request for `Empty`.
     public static func add(accountIDs: [String], toList id: String) -> Request<Empty> {
-        let parameter = accountIDs.map(toArrayOfParameters(withName: "account_ids"))
-        let method = HTTPMethod.post(.parameters(parameter))
+        let method = HTTPMethod.post(.json(encoding: ["account_ids": accountIDs]))
 
         return Request<Empty>(path: "/api/v1/lists/\(id)/accounts", method: method)
     }
@@ -85,8 +82,7 @@ public enum Lists {
     ///   - id: The list ID>
     /// - Returns: Request for `Empty`.
     public static func remove(accountIDs: [String], fromList id: String) -> Request<Empty> {
-        let parameter = accountIDs.map(toArrayOfParameters(withName: "account_ids"))
-        let method = HTTPMethod.delete(.parameters(parameter))
+        let method = HTTPMethod.delete(.json(encoding: ["account_ids": accountIDs]))
 
         return Request<Empty>(path: "/api/v1/lists/\(id)/accounts", method: method)
     }

Sources/MastodonKit/Requests/Notifications.swift
@@ -41,8 +41,7 @@ public enum Notifications {
     /// - Parameter id: The notification id.
     /// - Returns: Request for `Empty`.
     public static func dismiss(id: String) -> Request<Empty> {
-        let parameter = [Parameter(name: "id", value: String(id))]
-        let method = HTTPMethod.post(.parameters(parameter))
+        let method = HTTPMethod.post(.json(encoding: ["id": id]))
 
         return Request<Empty>(path: "/api/v1/notifications/dismiss", method: method)
     }

Sources/MastodonKit/Requests/Polls.swift
@@ -15,8 +15,7 @@ public enum Polls {
     }
 
     public static func vote(pollID: String, optionIndices: IndexSet) -> Request<Poll> {
-        let parameters = optionIndices.map(toArrayOfParameters(withName: "choices"))
-        let method = HTTPMethod.post(.parameters(parameters))
+        let method = HTTPMethod.post(.json(encoding: ["choices": optionIndices.map { "\($0)" }]))
         return Request<Poll>(path: "/api/v1/polls/\(pollID)/votes", method: method)
     }
 }

commit e2fe4f28daa19177f9f3f43eb30bf17dcaf718d0

change OAuth scopes back to be a space-separated string
According to https://docs.joinmastodon.org/entities/Token/#scope, this is correct. Apparently, this broke in https://github.com/chucker/MastodonKit/commit/4270aa7c87c1967fa82365d6197d6493cc77d301#diff-4be59296bdba05be3258942ac0d366a2877df8eaf878125b5e238d70350b3088.

Mastodon itself seems to handle this gracefully, but gotosocial does not: https://github.com/chucker/Mastonaut/issues/73

Sources/MastodonKit/Requests/Login.swift
@@ -27,7 +27,7 @@ public enum Login {
         let parameters: [String: AnyEncodable] = [
             "client_id": AnyEncodable(clientID),
             "client_secret": AnyEncodable(clientSecret),
-            "scope": AnyEncodable(scopes.map(toString)),
+            "scope": AnyEncodable(scopes.map(toString).joined(separator: " ")),
             "grant_type": AnyEncodable("password"),
             "username": AnyEncodable(username),
             "password": AnyEncodable(password)
@@ -54,7 +54,7 @@ public enum Login {
         let parameters: [String: AnyEncodable] = [
             "client_id": AnyEncodable(clientID),
             "client_secret": AnyEncodable(clientSecret),
-            "scope": AnyEncodable(scopes.map(toString)),
+            "scope": AnyEncodable(scopes.map(toString).joined(separator: " ")),
             "grant_type": AnyEncodable("authorization_code"),
             "redirect_uri": AnyEncodable(redirectURI),
             "code": AnyEncodable(code)

Tests/MastodonKitTests/Requests/LoginTests.swift
@@ -6,8 +6,8 @@
 //  Copyright © 2017 MastodonKit. All rights reserved.
 //
 
-import XCTest
 @testable import MastodonKit
+import XCTest
 
 class LoginTests: XCTestCase {
     func testSilentLogin() {
@@ -22,7 +22,7 @@ class LoginTests: XCTestCase {
 
         let payload = try! JSONSerialization.jsonObject(with: request.method.httpBody!, options: []) as! NSDictionary
         XCTAssertEqual(payload["username"] as? String, "foo")
-        XCTAssertEqual(payload["scope"] as? [String], ["read", "write"])
+        XCTAssertEqual(payload["scope"] as? String, "read write")
         XCTAssertEqual(payload["password"] as? String, "123")
         XCTAssertEqual(payload["client_id"] as? String, "client id")
         XCTAssertEqual(payload["client_secret"] as? String, "client secret")
@@ -42,7 +42,7 @@ class LoginTests: XCTestCase {
         let payload = try! JSONSerialization.jsonObject(with: request.method.httpBody!, options: []) as! NSDictionary
         XCTAssertEqual(payload["client_id"] as? String, "client id")
         XCTAssertEqual(payload["client_secret"] as? String, "client secret")
-        XCTAssertEqual(payload["scope"] as? [String], ["read", "write"])
+        XCTAssertEqual(payload["scope"] as? String, "read write")
         XCTAssertEqual(payload["grant_type"] as? String, "authorization_code")
         XCTAssertEqual(payload["redirect_uri"] as? String, "foo://oauth")
         XCTAssertEqual(payload["code"] as? String, "123")